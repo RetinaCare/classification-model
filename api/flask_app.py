de9199e5f5c97030a01de9d5d8902f10f5a8daa0
dev-xero <ssh.xero@gmail.com>
2025-11-24 15:52:05 +0100

    chore: clean-up flask app script
    Signed-off-by: dev-xero <ssh.xero@gmail.com>

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,74 +1,42 @@
+import io
+import sys
+import os
+import torch
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_SRC = os.path.join(BASE_DIR, "model", "src")
+sys.path.append(MODEL_SRC)
+
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
-import io
+from fusion_model import FusionModel
 
+# * ******************************************************
+# App Configs
+# * ******************************************************
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB
 
-# Configuration
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
-
-def allowed_file(filename):
-    """Check if the file has an allowed extension"""
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def validate_image_type(file_stream):
-    """Validate that the file is actually a JPEG image"""
-    try:
-        file_stream.seek(0)
-        
-        img = Image.open(file_stream)
-        
-        # Check if format is JPEG
-        is_jpeg = img.format == 'JPEG'
-        
-        file_stream.seek(0)
-        
-        return is_jpeg
-    except Exception:
-        file_stream.seek(0)
-        return False
-
-
+# * ******************************************************
+# Endpoints
+# * ******************************************************
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    # Check if image file is present in request
-    if 'image' not in request.files:
+    try:
+        filename = validate_request()
         return jsonify({
-            'success': False,
-            'error': 'No image file provided'
-        }), 400
-    
-    file = request.files['image']
-    
-    if file.filename == '':
+            'success': True,
+            'message': 'Image validated successfully',
+            'filename': filename
+        }), 200
+
+    except Exception as e:
         return jsonify({
-            'success': False,
-            'error': 'No file selected'
+            'success': True,
+            'message': repr(e)
         }), 400
-    
-    # Validate file extension
-    if not allowed_file(file.filename):
-        return jsonify({
-            'success': False,
-            'error': 'File must be a JPEG or JPG image'
-        }), 400
-    
-    # Validate actual file type
-    if not validate_image_type(file.stream):
-        return jsonify({
-            'success': False,
-            'error': 'File is not a valid JPEG image'
-        }), 400
-    
-    # If all validations pass
-    filename = secure_filename(file.filename)
-    return jsonify({
-        'success': True,
-        'message': 'Image validated successfully',
-        'filename': filename
-    }), 200
+
 
 # Test route to upload form
 @app.route('/', methods=['GET'])
@@ -88,6 +56,59 @@ def home():
     </body>
     </html>
     '''
+
+# * ******************************************************
+#  HELPER FUNCTIONS
+# * ******************************************************
+def validate_request():
+    if 'image' not in request.files:
+        raise Exception("No image file provided")
+
+    file = request.files['image']
+    if file.filename == '':
+        raise Exception("No file selected")
+
+    if not allowed_file(file.filename):
+        raise Exception("File must be a JPEG or JPG image")
+
+    if not validate_image_type(file.stream):
+        raise Exception("File is not a valid JPEG image")
+
+    return secure_filename(file.filename)
+
+
+def allowed_file(filename):
+    """Check if the file has an allowed extension"""
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def validate_image_type(file_stream):
+    """Validate that the file is actually a JPEG image"""
+    try:
+        file_stream.seek(0)
+        img = Image.open(file_stream)
+        is_jpeg = img.format == 'JPEG'
+        file_stream.seek(0)
+        return is_jpeg
+    except Exception:
+        file_stream.seek(0)
+        return False
+
+
+def load_model():
+    """
+    Loads classfication model from Digital Ocean Spaces.
+    """
+    try:
+        url = "https://s3.retinacare.ams3.digitaloceanspaces.com/fusion_model_mvp.pth"
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location="cpu")
+        model = FusionModel().to("cpu")
+        model.load_state_dict(state_dict)
+        model.eval()
+        return model
+    except Exception:
+        return False
 
 
 if __name__ == '__main__':
